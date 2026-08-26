f08b1b96cd5fcd863f25dceda948f4ca19868261
Ruthvik-N <sairuthvikn@gmail.com>
2026-08-26 17:47:12 +0530

    Add files via upload

---
+++ basics/01_sum.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+    int main() {
+        int a,b;
+        printf("Enter two numbers a and b:");
+        scanf("%d %d" , &a,&b);
+        printf("Sum of the numbers is:%d\n" , a + b);
+        return 0;
+    }
+    
