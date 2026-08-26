59ab27a36f2e46a10275725fe39a5e4ced5fd94f
Ruthvik-N <sairuthvikn@gmail.com>
2026-08-26 17:48:39 +0530

    Delete 02_area_square.c

--- 02_area_square.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a;
-    printf("enter the value of side:");
-    scanf("%d", &a);
-    printf("the area of the square required is:%d", a * a);
-    return 0;
-}
