0f819685f7ef1df68f43900ca0e20cb68010f2ed
Ruthvik-N <sairuthvikn@gmail.com>
2026-08-26 17:48:28 +0530

    Delete 01_sum.c

--- 01_sum.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-    int main() {
-        int a,b;
-        printf("Enter two numbers a and b:");
-        scanf("%d %d" , &a,&b);
-        printf("Sum of the numbers is:%d\n" , a + b);
-        return 0;
-    }
-    
